f8d9ccf07002c9446611167f05893d590b0309f6
gelunata <gelunata@gmail.com>
2025-12-06 15:50:57 +0300

    Added changes from practice at the middle level

---
+++ src/main/java/specs/ResponseSpecs.java
@@ -0,0 +1,36 @@
+package specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.ResponseSpecification;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
+public class ResponseSpecs {
+    private ResponseSpecs() {
+    }
+
+    private static ResponseSpecBuilder defaultRequestBuilder() {
+        return new ResponseSpecBuilder();
+    }
+
+    public static ResponseSpecification entityWasCreated() {
+        return defaultRequestBuilder()
+                .expectStatusCode(HttpStatus.SC_CREATED)
+                .build();
+    }
+
+    public static ResponseSpecification requestReturnsOK() {
+        return defaultRequestBuilder()
+                .expectStatusCode(HttpStatus.SC_OK)
+                .build();
+    }
+
+    public static ResponseSpecification requestReturnsBadRequest(String errorKey, String errorValue) {
+        return defaultRequestBuilder()
+                .expectStatusCode(HttpStatus.SC_BAD_REQUEST)
+                .expectBody(errorKey, Matchers.equalTo(errorValue))
+                .build();
+    }
+}
--- src/test/java/iteration1/CreateAccountTest.java
+++ src/test/java/iteration1/CreateAccountTest.java
@@ -1,84 +1,36 @@
 package iteration1;
 
-import io.restassured.RestAssured;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
-import io.restassured.http.ContentType;
-import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.BeforeAll;
+import generators.RandomData;
+import models.CreateUserRequest;
+import models.UserRole;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-import java.util.UUID;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasItem;
+import requests.AdminCreateUserRequester;
+import requests.CreateAccountRequester;
+import specs.RequestSpecs;
+import specs.ResponseSpecs;
 
 public class CreateAccountTest {
-    @BeforeAll
-    public static void setupRestAssured() {
-        RestAssured.filters(
-                List.of(new RequestLoggingFilter(),
-                        new ResponseLoggingFilter()));
-    }
 
     @Test
     public void userCanCreateAccountTest() {
-        // создание пользователя
-        String username = "user_" + UUID.randomUUID().toString().substring(0, 8);
-        String user = String.format("""
-                {
-                "username": "%s",
-                "password": "Qwerty1!",
-                "role": "USER"
-                }
-                """, username);
+        CreateUserRequest userRequest = CreateUserRequest.builder()
+                .username(RandomData.getUsername())
+                .password(RandomData.getPassword())
+                .role(UserRole.USER.toString())
+                .build();
 
-        given()
-                .contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-                .header("Authorization", "Basic YWRtaW46YWRtaW4=")
-                .body(user)
-                .post("http://localhost:4111/api/v1/admin/users")
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_CREATED);
+        new AdminCreateUserRequester(
+                RequestSpecs.adminSpec(),
+                ResponseSpecs.entityWasCreated())
+                .post(userRequest);
 
-        // получаем токен юзер
-        String userAuthHeader = given()
-                .contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-                .body(user)
-                .post("http://localhost:4111/api/v1/auth/login")
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_OK)
-                .extract()
-                .header("Authorization");
-
-        // создаем аккаунт (счет)
-        int id = given()
-                .header("Authorization", userAuthHeader)
-                .contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-                .post("http://localhost:4111/api/v1/accounts")
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_CREATED)
-                .extract()
-                .response()
-                .jsonPath()
-                .getInt("id");
+        new CreateAccountRequester(
+                RequestSpecs.authAsUser(
+                        userRequest.getUsername(),
+                        userRequest.getPassword()),
+                ResponseSpecs.entityWasCreated())
+                .post(null);
 
         // запросить все аккаунты пользователя и проверить, что наш аккаунт там
-        given()
-                .header("Authorization", userAuthHeader)
-                .contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-                .get("http://localhost:4111/api/v1/customer/accounts")
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_OK)
-                .body("id", hasItem(id));
     }
 }
